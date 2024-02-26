4af56fe1ccd7f2b68559aa78bad13af1ae4d2582
FrancescoVenanti <francesco.venanti@gmail.com>
2024-02-26 16:35:52 +0100

    Aggiungere i file di progetto.

---
+++ AziendaEdile/Controllers/HomeController.cs
@@ -0,0 +1,119 @@
+﻿using AziendaEdile.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AziendaEdile.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DbEdile"].ToString();
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            List<Dipendente> dipendenti = new List<Dipendente>();
+
+            try
+            {
+                conn.Open();
+
+                string query = "SELECT * FROM Dipendenti";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Dipendente dipendente = new Dipendente();
+                    dipendente.IDDipendente = Convert.ToInt32(reader["IDDipendente"]);
+                    dipendente.NomeDipendente = reader["NomeDipendente"].ToString();
+                    dipendente.CognomeDipendente = reader["CognomeDipendente"].ToString();
+                    dipendente.Indirizzo = reader["Indirizzo"].ToString();
+                    dipendente.CodFiscale = reader["CodFiscale"].ToString();
+                    dipendente.Sposato = Convert.ToBoolean(reader["Sposato"]);
+                    dipendente.FigliCarico = Convert.ToInt32(reader["FigliCarico"]);
+                    dipendente.Mansione = reader["Mansione"].ToString();
+
+                    dipendenti.Add(dipendente);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write($"Errore durante il recupero dei dati: {ex.Message}");
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return View(dipendenti);
+        }
+
+        public ActionResult InserisciDipendente()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult InserisciDipendente(Dipendente dipendente)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DbEdile"].ToString();
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            try
+            {
+                conn.Open();
+
+                string query =
+                    "INSERT INTO Dipendenti (NomeDipendente,CognomeDipendente,Indirizzo,CodFiscale,Mansione,Sposato,FigliCarico) " +
+                    "VALUES (@NomeDipendente,@CognomeDipendente,@Indirizzo,@CodFiscale,@Mansione,@Sposato,@FigliCarico) ";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@NomeDipendente", dipendente.NomeDipendente);
+                cmd.Parameters.AddWithValue("@CognomeDipendente", dipendente.CognomeDipendente);
+                cmd.Parameters.AddWithValue("@Indirizzo", dipendente.Indirizzo);
+                cmd.Parameters.AddWithValue("@CodFiscale", dipendente.CodFiscale);
+                cmd.Parameters.AddWithValue("@Mansione", dipendente.Mansione);
+                cmd.Parameters.AddWithValue("@Sposato", dipendente.Sposato);
+                cmd.Parameters.AddWithValue("@FigliCarico", dipendente.FigliCarico);
+
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("aaaaaaaaaaaa" +ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        
+    }
+}
---
+++ AziendaEdile/Models/Dipendente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AziendaEdile.Models
+{
+    public class Dipendente
+    {
+        public int IDDipendente { get; set; }
+        public string NomeDipendente { get; set; }
+        public string CognomeDipendente { get; set; }
+        public string Indirizzo { get; set; }
+        public string CodFiscale { get; set; }
+        [Display(Name = "E' Sposato?")]
+        public bool Sposato { get; set; }
+        public int FigliCarico { get; set; }
+        public string Mansione { get; set; }
+
+        public Dipendente() { }
+
+    }
+}
